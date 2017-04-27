1cdcd0531445beb64746eeb6cc229f53b657575e
Homting Li <Homting@users.noreply.github.com>
2017-04-27 20:08:54 +0200

    Update knn.py
    change some function name, and add training() function

--- knn.py
+++ knn.py
@@ -12,32 +12,34 @@ from readData import readData
 class knn():
     __attributes = []
     __instances = []
+    __norm = []
     __trainSet = []
     __testSet = []
     __k = 3
+    __split = 0.8
 
     # read data set
-    def __init__(self, filename, split,k):
+    def __init__(self, filename, split,k=3):
         self.__filename = filename
         self.__k = k
+        self.__split = split
         f = readData(self.__filename)
         self.__attributes,self.__instances = f.readDataSet()
-        self.__trainSet,self.__testSet = self.splitDataSet(split)
-        self.getPrediction()
-
-    # divide dataset into training and testing dataset
-    def splitDataSet(self,split):
-        trainSet = []
-        testSet = []
         for i in range(len(self.__instances)):
             for j in range(len(self.__attributes)):
                 self.__instances[i][j] = float(self.__instances[i][j])
 
-        for i in range(len(self.__instances)):
+
+    #divide dataset into training and testing dataset
+    def __splitDataSet(self, data, split):
+        trainSet = []
+        testSet = []
+
+        for i in range(len(data)):
             if random() <= split:
-                trainSet.append(self.__instances[i])
+                trainSet.append(data[i])
             else:
-                testSet.append(self.__instances[i])
+                testSet.append(data[i])
         return trainSet, testSet
 
 
@@ -48,8 +50,32 @@ class knn():
     def getInstances(self):
         return self.__instances
 
+    def getNormalization(self):
+        if len(self.__norm)==0:
+            self.__normalization()
+        return self.__norm
+
+    def __normalization(self):
+        res = []
+        for column in range(len(self.__instances)):
+            l = len(self.__attributes)
+            temp = self.__instances[column][:]
+
+            max_val, min_val = max(temp[:l-1]), min(temp[:l-1])
+            #print(min_val)
+
+            if min_val!=max_val:
+                t = [float(i)/sum(temp[:l-1]) for i in temp[:l-1]]
+                t.append(temp[l])
+                res.append(t)
+            else:
+                res.append(temp)
+
+        return res
+
+
     # to calculate the Euclidean distance
-    def getEuclideanDistance(self, data1, data2):
+    def __getEuclideanDistance(self, data1, data2):
         d = 0.0
         for x in range(len(data1)-1):
             d += pow((data1[x] - data2[x]),2)
@@ -58,37 +84,41 @@ class knn():
 
     # get K near neighborhoods
     # 获取还有点问题, 字典会导致数据缺失
-    def getKNearNeighbors(self, trainSet, testInstance, n):
+    def __getKNearNeighbors(self, trainSet, testInstance, n=3):
         distances = []
         neighbors = []
-        n2 = []
+        neighbors2 = []
         dis = {}
 
         for i in range(len(trainSet)):
-            dist = self.getEuclideanDistance(testInstance,trainSet[i])
+            dist = self.__getEuclideanDistance(testInstance,trainSet[i])
             distances.append((trainSet[i],dist))
-            dis.setdefault(tuple(trainSet[i]),dist)
+
+            xxx = trainSet[i][:];xxx.append(i)
+            dis.setdefault(tuple(xxx),dist)
 
         distances.sort(key=itemgetter(1))
 
-        temp = sorted(dis.items(),key=itemgetter(1))
+        dis = sorted(dis.items(),key=itemgetter(1))
 
         for i in range(n):
             neighbors.append(distances[i][0])
-            n2.append(list(temp[i][0]))
+            ttt = list(dis[i][0])
+            neighbors2.append(ttt[:len(ttt)-1])
 
-            if (neighbors!=n2):
+            if (neighbors!=neighbors2):
                 print(neighbors,end=",")
                 print(distances[i][1])
                 print(len(distances))
-                print(n2,end=",")
-                print(temp[i][1])
-                print(len(temp))
+                print(neighbors2,end=",")
+                print(dis[i][1])
+                print(len(dis))
                 print()
-        return n2  #使用 neighbors最好
+        #return n2  #使用 neighbors最好
+        return neighbors2
 
 
-    def getResult(self, neighbors):
+    def __getClassification(self, neighbors):
         dic = {}
         for i in range(len(neighbors)):
             response = neighbors[i][-1]   # result
@@ -99,7 +129,7 @@ class knn():
         return temp[0][0]
 
 
-    def getAccuracy(self, testSet, predictions):
+    def __getResult(self, testSet, predictions):
         correct = 0
         for i in range(len(testSet)):
             if testSet[i][-1] == predictions[i]:
@@ -108,12 +138,14 @@ class knn():
         return (correct/float(len(testSet))) *100.0
 
 
-    def getPrediction(self):
+    def training(self):
         predictions = []
+        #self.__trainSet,self.__testSet = self.__splitDataSet(self.__instances,self.__split)
+        self.__trainSet,self.__testSet = self.__splitDataSet(self.__normalization(),self.__split)
+
         for i in range(len(self.__testSet)):
-            neighbors = self.getKNearNeighbors(self.__trainSet,self.__testSet[i],self.__k)
-            result = self.getResult(neighbors)
+            neighbors = self.__getKNearNeighbors(self.__trainSet,self.__testSet[i],self.__k)
+            result = self.__getClassification(neighbors)
             predictions.append(result)
         #print(result)
-        accuracy = self.getAccuracy(self.__testSet,predictions)
-        print(accuracy)
+        return self.__getResult(self.__testSet,predictions)
