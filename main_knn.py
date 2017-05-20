348008fecac213ca33f125cdb4c7582df103a842
Homting Li <Homting@users.noreply.github.com>
2017-05-21 00:13:45 +0200

    Update main_knn.py
    如果测试集 没有 结果的话, 没有最后一列的话, 就不能得到getAccuracy

--- main_knn.py
+++ main_knn.py
@@ -12,6 +12,10 @@ attributes,instences = tk.readDataSet("dataset/iris.txt")
 
 #trainSet,testSet = tk.splitDataSet(instences,0.8)
 #myknn = knn(attributes,trainSet,testSet,3)
+new_testSet = []
+for t in testSet:
+	new_testSet.append(t[:len(attributes)])
+print(new_testSet)
 
 lst = []
 for i in range(1):
