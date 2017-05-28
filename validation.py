5ff23af8a5c0a400eaff02e88a8c0c63864f2ef7
Jin LI <Homting@users.noreply.github.com>
2017-05-28 11:40:01 +0200

    Update validation.py
    id3

--- validation.py
+++ validation.py
@@ -2,6 +2,7 @@ import toolkit as tk
 import pandas as pd
 import numpy as np
 from AI_knn import *
+from AI_id3 import *
 from naivebayes.naive_bayes import *
 
 # Leave one out procedure
@@ -21,7 +22,6 @@ def leave_1_ins(dataset, index):
 
 def cross_vali_split_data(dataset, cv=10):
     '''
-
     :param dataset: type matrix
     :param cv: the number of validation fold
     :return: K-1 length list contain the dataset
@@ -49,26 +49,25 @@ def accuracy_score(right, predict):
     return num / len(right)
 
 
-def mcc(predict, right, stru):
-    TP, FP, FN, TN = 0, 0, 0, 0
+def kappa(predict, right,classi):
+    #TP, FP, FN, TN = 0, 0, 0, 0
+    a,b, c, d = 0, 0, 0, 0
     for i in range(len(predict)):
-        if predict[i] == stru:
-            if right[i] == stru:
-                TP += 1
+        if predict[i] == classi:
+            if right[i] == classi:
+                a += 1
             else:
-                FP += 1
+                b += 1
         else:
-            if right[i] == stru:
-                FN += 1
+            if right[i] == classi:
+                c += 1
             else:
                 if predict[i]==right[i]:
-                    TN += 1
-    print(TP,FP,FN,TN)
-    MCC = (TP * TN - FP * FN) / np.sqrt((TP + FP) * (TP + FN) * (TN + FP) * (TN + FN))
-    TRP = TP / (TP + FN)
-    SPC = TN / (FP + TN)
+                    d += 1
+    print(a,b,c,d)
+    kappa = ((a+d)/(a+b+c+d)-((a+c)*(a+b)+(b+d)*(c+d))/(a+b+c+d)**2)/(1.0-((a+c)*(a+b)+(b+d)*(c+d))/(a+b+c+d)**2)
 
-    return MCC
+    return kappa
 
 
 # test cross validation
@@ -88,26 +87,68 @@ def cross(model,attr,dataset,cvfold=10):
     right = dataset[:,-1]
     train_test_pair_list = cross_vali_split_data(dataset,cvfold)
     prediction = []
-    for train_test in train_test_pair_list:
-        mod = model(attr,train_test[0])
-        for i in mod.getPrediction(train_test[1])[1]:
-            prediction.append(i)
-    print(right)
-    print(prediction)
+    if model==id3:
+        for train_test in train_test_pair_list:
+            mod = model(list(attr),(train_test[0]).tolist())
+            for i in mod.getPrediction(train_test[1].tolist())[1]:
+                prediction.append(i)
+    else:
+        for train_test in train_test_pair_list:
+            mod = model(attr,train_test[0])
+            for i in mod.getPrediction(train_test[1])[1]:
+                prediction.append(i)
+    #print(right)
+    #print(prediction)
     score = accuracy_score(right,prediction)
-    return score
+    return score,right,prediction
 
 
-'''
-dfiris = pd.read_csv("./iris.csv")
-col= dfiris.columns
-a= dfiris.sample(frac=1).reset_index()
+att, x = tk.readDataSet("./iris.csv")
 
-print(a)
+a,right,pre = cross(id3,att,x,10)
+a,right,pre = cross(knn,att,x,10)
 
-'''
 car_attr,car = tk.readDataSet("./car.csv")
-print(cross(Naive_bayes,car_attr,car))
+#a,right,pre = cross(Naive_bayes,car_attr,car)
+
+def kappa_dict(predict,right):
+    key_list = []
+    for i in right:
+        if i in key_list:
+            pass
+        else:
+            key_list.append(i)
+
+    k_value = {}
+    for i in key_list:
+        k_value[i] = kappa(predict,right,i)
+    return k_value
+
+print(kappa_dict(pre,right))
+
+
+def classify_map(predict,right):
+    key_list = []
+    for i in right:
+        if i in key_list:
+            pass
+        else:
+            key_list.append(i)
+
+    map_dict = {}
+    for i in key_list:
+        dict_t ={}
+        for j in key_list:
+            dict_t[j] = 0
+        map_dict[i] = dict_t
+    for i in range:
+        map = pd.DataFrame(map_dict)
+    return map
+
+#print(classify_map(pre,right))
+
+
+
 
 
 
