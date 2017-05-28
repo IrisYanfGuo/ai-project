6b51862cbfc50fb05d0cfc07d7eb7b96c60eac2c
IrisGuo <yanfguo@vub.ac.be>
2017-05-28 15:43:10 +0200

    Merge branch 'master' of https://github.com/Homting/ai-project
    # Conflicts:
    #	validation.py

--- validation.py
+++ validation.py
@@ -3,6 +3,11 @@ import pandas as pd
 import numpy as np
 from AI_knn import *
 from naivebayes.naive_bayes import *
+import seaborn as sns
+import matplotlib.pyplot as plt
+from naivebayes.naive_bayes_conti import *
+from AI_id3 import *
+
 
 # Leave one out procedure
 
@@ -37,7 +42,7 @@ def cross_vali_split_data(dataset, cv=10):
         begin = end
         end = end + size
         result.append([trainset, testset])
-    result.append([ dataset[:begin, :],dataset[begin:, :]])
+    result.append([dataset[:begin, :], dataset[begin:, :]])
     return result
 
 
@@ -49,65 +54,94 @@ def accuracy_score(right, predict):
     return num / len(right)
 
 
-def mcc(predict, right, stru):
-    TP, FP, FN, TN = 0, 0, 0, 0
+def kappa(predict, right, classi):
+    a, b, c, d = 0, 0, 0, 0
     for i in range(len(predict)):
-        if predict[i] == stru:
-            if right[i] == stru:
-                TP += 1
+        if predict[i] == classi:
+            if right[i] == classi:
+                a += 1
             else:
-                FP += 1
+                b += 1
         else:
-            if right[i] == stru:
-                FN += 1
+            if right[i] == classi:
+                c += 1
             else:
-                if predict[i]==right[i]:
-                    TN += 1
-    print(TP,FP,FN,TN)
-    MCC = (TP * TN - FP * FN) / np.sqrt((TP + FP) * (TP + FN) * (TN + FP) * (TN + FN))
-    TRP = TP / (TP + FN)
-    SPC = TN / (FP + TN)
+                if predict[i] == right[i]:
+                    d += 1
+    print(a, b, c, d)
+    kappa = ((a + d) / (a + b + c + d) - ((a + c) * (a + b) + (b + d) * (c + d)) / (a + b + c + d) ** 2) / (
+    1.0 - ((a + c) * (a + b) + (b + d) * (c + d)) / (a + b + c + d) ** 2)
 
-    return MCC
-
-
-# test cross validation
-att, x = tk.readDataSet("./iris.csv")
-a = cross_vali_split_data(x, 16)
-right1 = x[:,-1]
-pre = []
-#for i in a:
- #   res = knn(att,i[0])
-  #  for i in res.getPrediction(i[1])[1]:
-   #     pre.append(i)
+    return kappa
 
 
+def kappa_dict(predict, right):
+    key_list = []
+    for i in right:
+        if i in key_list:
+            pass
+        else:
+            key_list.append(i)
 
-def cross(model,attr,dataset,cvfold=10):
+    k_value = {}
+    for i in key_list:
+        k_value[i] = kappa(predict, right, i)
+    return k_value
+
+
+def cross(model, attr, dataset, cvfold=10):
     np.random.shuffle(dataset)
-    right = dataset[:,-1]
-    train_test_pair_list = cross_vali_split_data(dataset,cvfold)
+    right = dataset[:, -1]
+    train_test_pair_list = cross_vali_split_data(dataset, cvfold)
     prediction = []
-    for train_test in train_test_pair_list:
-        mod = model(attr,train_test[0])
-        for i in mod.getPrediction(train_test[1])[1]:
-            prediction.append(i)
-    print(right)
-    print(prediction)
-    score = accuracy_score(right,prediction)
-    return score
+    if model == id3:
+        for train_test in train_test_pair_list:
+            mod = model(list(attr), (train_test[0]).tolist())
+            for i in mod.getPrediction(train_test[1].tolist())[1]:
+                prediction.append(i)
+    else:
+        for train_test in train_test_pair_list:
+            mod = model(attr, train_test[0])
+            for i in mod.getPrediction(train_test[1])[1]:
+                prediction.append(i)
+
+    score = accuracy_score(right, prediction)
+    k_value = kappa_dict(prediction, right)
+    classi_map = classify_map(prediction, right)
+    return score, k_value, classi_map
 
 
-'''
-dfiris = pd.read_csv("./iris.csv")
-col= dfiris.columns
-a= dfiris.sample(frac=1).reset_index()
+def classify_map(predict, right):
+    key_list = []
+    for i in right:
+        if i in key_list:
+            pass
+        else:
+            key_list.append(i)
 
-print(a)
+    map_dict = {}
+    for i in key_list:
+        dict_t = {}
+        for j in key_list:
+            dict_t[j] = 0
+        map_dict[i] = dict_t
+    for i in range(len(right)):
+        map_dict[right[i]][predict[i]] += 1
+    print(map_dict)
 
-'''
-car_attr,car = tk.readDataSet("./car.csv")
-print(cross(Naive_bayes,car_attr,car))
+    map = pd.DataFrame(map_dict)
+    return map
 
 
+def draw_map(map, xlab="predict result", ylab="right "):
+    sns.heatmap(map, annot=True)
+    sns.plt.xlabel(xlab)
+    sns.plt.ylabel(ylab)
+    plt.show()
 
+
+car_attr, car = tk.readDataSet("./iris.csv")
+a, k_va, iris_map = cross(knn, car_attr, car)
+
+draw_map(iris_map)
+print(k_va)
